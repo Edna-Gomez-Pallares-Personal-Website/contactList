55e74ef08b3528546bc92dc1cb94d91afc0c55d7
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-01-31 00:10:02 -0500

    setting up the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import ContactList from './components/ContactList';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return 
-  
-}
-
-export default App
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+};
