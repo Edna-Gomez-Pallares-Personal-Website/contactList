6a977d41f47c925e4b8ac5a2b59efc3928e7c2ee
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-02-05 16:40:59 -0500

    Finishing up

--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,11 +1,14 @@
 export default function ContactRow({ setSelectedContactId, contact }) {
+  
     return (
-      <tr onClick={() => {
-        setSelectedContactId(contact.id);
-      }}>
-        <td>{contact.name}</td>
-        <td>{contact.email}</td>
-        <td>{contact.phone}</td>
-      </tr>
-    );
-  }
+        <tr
+          onClick={() => {
+            setSelectedContactId(contact.id);
+          }}
+        >
+          <td>{contact.name}</td>
+          <td>{contact.email}</td>
+          <td>{contact.phone}</td>
+        </tr>
+      );
+    }
